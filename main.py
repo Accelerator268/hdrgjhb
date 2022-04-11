bae3917de3d327e7de95e92f12d797e34d224a3a
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-11 01:10:36 +0000

    famous(8)

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-income = int(input())
-if income <= 15527:
-  calculated_tax = 0
-  print('{0} {income} {1} {calculated_tax} {2}'.format('The tax for', 'is 0%. That is', 'dollars!', income = income, calculated_tax = calculated_tax))
-elif 15528 <= income <= 42707:
-  calculated_tax = income * 15 // 100
-  print('{0} {income} {1} {calculated_tax} {2}'.format('The tax for', 'is 15%. That is', 'dollars!', income = income, calculated_tax = calculated_tax))
-elif 42708 < income < 132406:
-  calculated_tax = income * 25 // 100
-  print('{0} {income} {1} {calculated_tax} {2}'.format('The tax for', 'is 25%. That is', 'dollars!', income = income, calculated_tax = calculated_tax))
-elif 132407 < income:
-  calculated_tax = income * 28 // 100
-  print('{0} {income} {1} {calculated_tax} {2}'.format('The tax for', 'is 28%. That is', 'dollars!', income = income, calculated_tax = calculated_tax))
+#Angelina Jolie and Leonardo DiCaprio met yesterday in Paris.
+print('{0} and {2} met yesterday in {1}.'.format('Angelina Jolie', 'Paris', 'Leonardo DiCaprio'))
