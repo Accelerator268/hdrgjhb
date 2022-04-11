8b339168d2b4a2564cf2d34e3509e7aeb76fa31f
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-11 02:48:22 +0000

    slicing 3

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-series = ['Game of Thrones', 'Riverdale', 'Shameless', 'Doctor Who', 'Friends']
-print(series[3:5])
-
-#['Doctor Who', 'Friends']
+sequence = 'kjsf'
+print(sequence[0:len(sequence):1])
