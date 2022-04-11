d44ea5ea645ea04f8255030bf392aa1397b556cb
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-11 02:39:56 +0000

    domino (stage 1) UPD2

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import random
 
-list = [[0, 0], [0, 1], [0, 2], [0, 3],[0, 4], [0, 5], [0, 6], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [3, 3], [3, 4], [3, 5], [3, 6], [4, 4], [4, 5], [4, 6], [5, 5], [5, 6], [6, 6]]
+
+list = []
+for i in range(7):
+    for j in range(i, 7):
+        list.append([i, j])
 domino_snake = []
 while ([6, 6] not in domino_snake) and ([5, 5] not in domino_snake):
     stock_pieces, computer_pieces, player_pieces = list.copy(), [], []
