427d9f464b06545533a84bb373e2764053012a96
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-11 01:08:52 +0000

    tax (7)

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# % foramatting
-print('%.3f' % (11 /  3))
-name = 'Mary'; age = 15
-print('Hi %s you are %d' % (name, age))
-
-# .format()
-print('{} {} {}'.format('Me', 'and', 'You'))
-print('{who} {} {}'.format('Me', 'and', who='You'))
-print('{who} {} {}'.format('Me', 'and', who='You'))
-print('{2} {1} {0}'.format('Me', 'and', 'You'))
-
-# f'{var]'
-a = 5
-print(f'%.{a}f' % (11 /  3))
-name = 'Jonny Cage'
-print(f'This is the end of you, {name}')
+income = int(input())
+if income <= 15527:
+  calculated_tax = 0
+  print('{0} {income} {1} {calculated_tax} {2}'.format('The tax for', 'is 0%. That is', 'dollars!', income = income, calculated_tax = calculated_tax))
+elif 15528 <= income <= 42707:
+  calculated_tax = income * 15 // 100
+  print('{0} {income} {1} {calculated_tax} {2}'.format('The tax for', 'is 15%. That is', 'dollars!', income = income, calculated_tax = calculated_tax))
+elif 42708 < income < 132406:
+  calculated_tax = income * 25 // 100
+  print('{0} {income} {1} {calculated_tax} {2}'.format('The tax for', 'is 25%. That is', 'dollars!', income = income, calculated_tax = calculated_tax))
+elif 132407 < income:
+  calculated_tax = income * 28 // 100
+  print('{0} {income} {1} {calculated_tax} {2}'.format('The tax for', 'is 28%. That is', 'dollars!', income = income, calculated_tax = calculated_tax))
