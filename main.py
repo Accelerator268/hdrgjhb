4144354e7be1568687dcddf28ad3a615612301e0
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-12 23:04:49 +0000

    slicing(6)

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-from random import randint
-
-
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'April', 'September', 'October', 'November', 'December']
-revenues = [randint(500, 2000) for _ in range(12)]
-profit = ['n/a']
-conclusion = ['n\a', 'great', 'decent', 'need follow up', 'critical']
-
-for i in range(1, 12):
-  profit.append((revenues(i) - revenues(i-1))/revenues(i)*100)
-
+s = [i for i in range(101)]
+print(s[5::5])
