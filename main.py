76af71323640e7b6121e421fa28ccd464f8f156e
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-12 02:25:48 +0000

    iterators

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-from string import ascii_lowercase
-from random import randint
+# iterators
 
-abc = [ascii_lowercase[randint(0, 25)] for i in range(randint(10, 20))]
-vowels = ['a', 'e', 'i', 'o', 'u']
-qwerty = list(filter(lambda x: x in vowels, abc))
-print(abc)
-print(qwerty)
+# iterators use dunders __iter()__ & __next()__
 
-#'y' в расчет не идет, по заданию должно быть 5 гласных
+my_list = [1, 2, 3]
+my_iter = iter(my_list)  # turn list to generator
+print(my_iter)
+print(next(my_iter))  # get intance in generator
+print(next(my_iter))  # get next intance in generator till error
+
+for a in my_list:  # same concept
+  print(a)
+
+
+# zip
+
+names = ['Mary', 'Ann']
+surnames = ['Smith', 'Boul']
+for name, surname in zip(names, surnames):
+  print(name, surname)
+
+names = ['Mary', 'Ann', 'Bob']
+surnames = ['Smith', 'Boul']
+for name, surname in zip(names, surnames):  # stops at shortest
+  print(name, surname)
+
+# from python 3.10 you can make sure lists are identical in length
+names = ['Mary', 'Ann', 'Bob']
+surnames = ['Smith', 'Boul']
+for name, surname in zip(names, surnames, strict=True):  # throws error
+  print(name, surname)
+  
+# enumerate
+  
+names = ['Mary', 'Ann', 'Bob']
+
+i = 1
+for name in names:
+  print(i, name)
+
+for i, name in enumerate(names):
+  print(i + 1, name)
+# or
+for i, name in enumerate(names, start=1):
+  print(i, name)
