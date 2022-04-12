e83f578879f05367871be5e153f92d0f386710c4
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-12 00:37:10 +0000

    binary search (1)

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-mail = input()
-if '@' in mail:
-  print(mail[:mail.find('@')])
+import random
+
+#для сдвига берем в расчет только положительные числа
+move = random.randint(1, 10)
+print(move)
+a_sorted = [random.randint(0, 10) for _ in range(10)]
+a_sorted.sort()
+a_new = []
+if move > len(a_sorted):
+    move %= len(a_sorted)
+for i in range(len(a_sorted)):
+    a_new.append(a_sorted[i - move])
+print('{} {a_sorted}'.format('отсортированный массив:', a_sorted = a_sorted))
+print('{} {a_new}'.format('сдвинутый массив:', a_new = a_new))
+print('{} {maximum}'.format('максимум массива:', maximum = a_new[move-1]))
