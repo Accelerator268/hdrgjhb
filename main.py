4c3938fe57a45858e6401d7f94c045e11169ce60
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-12 23:19:19 +0000

    arithmetic(1)

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-my_list = [3, 9, 27, 81, 243, 729, 2187]
-#my_list[:] == [3, 9, 27, 81, 243, 729, 2187]
-#my_list[-1:] == [2187]
-#my_list[2:-1:-3] == []
-#my_list[7] == IndexError
-#my_list[2:0:-3] == [27]
+n, k = map(int, (input(), input()))
+print((n+k)*(n-k))
