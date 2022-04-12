16c1db869e60da3dc42c93e3d8c3e3bb0d409601
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-12 02:59:43 +0000

    iterator(3) BETA

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-s1 = list(input())
-s2 = list(input())
-for s1, s2 in zip(s1, s2):
-  print(s1, s2, sep = '', end = '')
+from random import randint
+
+
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'April', 'September', 'October', 'November', 'December']
+revenues = [randint(500, 2000) for _ in range(12)]
+profit = ['n/a']
+conclusion = ['n\a', 'great', 'decent', 'need follow up', 'critical']
+
+for i in range(1, 12):
+  profit.append((revenues(i) - revenues(i-1))/revenues(i)*100)
+
