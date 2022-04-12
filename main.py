56b9eb5c04ba885eb94ac1b2a4fcf6f961d386e9
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-12 02:24:05 +0000

    vowels

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from random import randint
 
 abc = [ascii_lowercase[randint(0, 25)] for i in range(randint(10, 20))]
 vowels = ['a', 'e', 'i', 'o', 'u']
-print([i for i in abs if i in vowels])
+qwerty = list(filter(lambda x: x in vowels, abc))
+print(abc)
+print(qwerty)
+
+#'y' в расчет не идет, по заданию должно быть 5 гласных
