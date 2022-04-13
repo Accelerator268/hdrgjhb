72a3a8e705035d0dac85259e7cadc6a959a7181e
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-13 00:59:31 +0000

    exeptions

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-num = [i for i in range(10)]
-even = list(filter(lambda x: x%2==0, num))
-odd = list(filter(lambda x: x%2, num))
+'''Python Workshop DAY 3
+1 Project Stage 2 (enumerate!!)
+2 Binary Search!!!
+3 Primes - new (folder 06)
+4 Exceptions and Handlers- Theory and Practice
+'''
 
-my_sum = 0
-i = 0
-my_sum = list(map(lambda x, y: x + y, even, odd))
 
-reminders = list(map(lambda x: x%3, my_sum))
 
-nonzero_reminders = list(filter(lambda x: x, reminders))
+print('Hello')
+print(10 / 0)  # code goes till 
 
-print(nonzero_reminders)
+# typical
+
+# NameError
+a = 5; print(a + b)  # b is not defined before call
+
+# TypeError
+print('15' + 2)  # operatio
