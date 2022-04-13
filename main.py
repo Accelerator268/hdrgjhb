e3e57119dc3ac55b9b5eeeaabb3f5d82b51cc384
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-13 23:48:24 +0000

    iterator(5)

--- main.py
+++ main.py
@@ -1,55 +1,7 @@
-# Handling Exceptions
-
-# list
-# https://docs.python.org/3/library/exceptions.html
-
-a = 5
-b = 2
-
-print(a / b)  # work as expected
-
-
-a = 5
-b = 0
-
-print(a / b)  # ZeroDivision
-
-# solution
-try:
-  result = a / b
-except ZeroDivisionError as err:
-  print('0 Error', err)  # err is exception description
-else:
-  print('The result is ', result)
-finally:
-  print("Thank you for using the service")
-
-  
-a = 5
-b = 'five'
-
-print(a / b)  # TypeError
-
-# solution
-try:
-  result = a / b
-except ZeroDivisionError as err:
-  print('0 Error', err)  # err is exception description
-except TypeError as err:
-  print('T Error', err)  # err is exception description
-else:
-  print('The result is ', result)
-finally:
-  print("Thank you for using the service")
-
-# line num of exception
-import sys 
-e_type, e_object, e_traceback = sys.exc_info()
-e_line = e_traceback.tb_lineno
-
-
-# exception name
-# use __class__.__name__ called on err object
-
-# multiple exceptions
-# except (ZeroDivisionError, TypeError) as err:
+xyz1 = ', '.join(input().split())
+xyz2 = ', '.join(input().split())
+print('v1 = (', xyz1, ') & v2 = (', xyz2, ')', sep = '')
+xyz1 = xyz1.split(', ')
+xyz2 = xyz2.split(', ')
+for xy, xz in zip(xyz1, xyz2):
+  print(int(xy) + int(xz))
