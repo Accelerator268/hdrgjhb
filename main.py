9f3ae38944269be058afb2daee4fc39bad63ab6b
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-13 03:00:42 +0000

    iterator(3) BETA

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def positive(list1):
-  return list(filter(lambda x: x>0, list1))
+from random import randint
 
 
-list1 = [1, 2, 3, 4, -5, -9, 0]
-print(positive(list1))
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'April', 'September', 'October', 'November', 'December']
+revenues = [randint(500, 2000) for _ in range(12)]
+profit = ['n/a']
+conclusion = ['n\a', 'great', 'decent', 'need follow up', 'critical']
+
+for i in range(1, 12):
+  profit.append('%.1f' % ((revenues[i] - revenues[i-1])*100/revenues[i]))
+
+for i in range(12):
+  print(months[i], revenues[i], profit[i]+'%')
+  if i == 0:
+    print(conclusion[0])
+  elif profit[i]>50:
+    print(conclusion[1])
+  elif 50>=profit[i]>=25:
+    print(conclusion[2])
+  elif 25>profit[i]>=0:
+    print(conclusion[3])
+  elif profit[i]<0:
+    print(conclusion[4])
