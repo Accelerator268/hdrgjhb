693e62910312b4e33074ecffa43ba6be2e4b1502
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-13 02:13:18 +0000

    map & filter (3)

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-from random import randint, shuffle
+from random import randint
 
-
-stock = [[i, j] for i in range(7) for j in range(i, 7)]
-doubles = stock[:4:-2]
-on_hands = 7
-
-while not any([double in stock[:on_hands * 2] for double in doubles]):
-    shuffle(stock)
-
-for double in doubles:
-    if double in stock[:on_hands * 2]:
-        snake = stock.pop(stock.index(double))
-        break
-
-computer, player = stock[:on_hands - 1], stock[on_hands - 1:on_hands*2-1]
-if randint(1, 2) == 2:
-    player, computer = computer, player
-
-stock = stock[on_hands * 2 - 1:]
-
-next_one = 'computer' if len(computer) > len(player) else 'player'
-
-print('   Stock pieces: {} \n\
-Computer pieces: {} \n\
-  Player pieces: {} \n\
-   Domino snake: [{}] \n\
-         Status: {}'.\
-format(stock, computer, player, snake, next_one))
-
-print('=' * ord('F'))
-print('Stock size: %s\nComputer pieces: %s\n\n%s\n' % (len(stock), len(computer), snake))
-print('Your pieces:\n{}'.format('\n'.join(list(f'{i + 1}:{e}' for i, e in enumerate(list(map(str, ([pcs for pcs in player]))))))))
-print(''.join(chr(n) for n in [83, 116, 97, 116, 117, 115, 58]), end = ' ')
-print('Computer is about to make a move. Press Enter to continue...' if len(computer) > len(player) else \
-      'It\'s your turn to make a move. Enter your command.')
+student_list = ['Denis', 'Dima', '???', 'Dima G.', 'Dima T.', 'Alexey', 'X3', 'NN']
+student_points_math = [randint(25, 50) for _ in range(len(student_list))]
+student_points_phys = [randint(25, 50) for _ in range(len(student_list))]
+student_points_eng = [randint(25, 50) for _ in range(len(student_list))]
+student_pass = list(map(lambda x, y, z: x + y + z, student_points_math, student_points_phys, student_points_eng))
+print(student_list)
+print(student_pass)
+print([student_list[i] for i in range(len(student_pass)) if student_pass[i]>=100])
