74adac471a3fcf2bbccc231c289ed73eab1f7cbe
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-13 23:32:50 +0000

    handling exeptions

--- main.py
+++ main.py
@@ -1,23 +1,55 @@
-from random import randint
+# Handling Exceptions
+
+# list
+# https://docs.python.org/3/library/exceptions.html
+
+a = 5
+b = 2
+
+print(a / b)  # work as expected
 
 
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-revenues = [randint(500, 2000) for _ in range(12)]
-profit = ['n/a']
-conclusion = ['n\a', 'great', 'decent', 'need follow up', 'critical']
+a = 5
+b = 0
 
-for i in range(1, 12):
-  profit.append('%.1f' % ((revenues[i] - revenues[i-1])*100/revenues[i]))
+print(a / b)  # ZeroDivision
 
-for i in range(12):
-  print(months[i], revenues[i], profit[i], end = '')
-  if i == 0:
-    print(conclusion[0])
-  elif revenues[i]/revenues[i-1] > 1.5:
-    print('%', conclusion[1])
-  elif 1.5 >= revenues[i]/revenues[i-1] >= 1.25:
-    print('%', conclusion[2])
-  elif 1.25 > revenues[i]/revenues[i-1] >= 1:
-    print('%', conclusion[3])
-  elif revenues[i]/revenues[i-1] < 1:
-    print('%', conclusion[4])
+# solution
+try:
+  result = a / b
+except ZeroDivisionError as err:
+  print('0 Error', err)  # err is exception description
+else:
+  print('The result is ', result)
+finally:
+  print("Thank you for using the service")
+
+  
+a = 5
+b = 'five'
+
+print(a / b)  # TypeError
+
+# solution
+try:
+  result = a / b
+except ZeroDivisionError as err:
+  print('0 Error', err)  # err is exception description
+except TypeError as err:
+  print('T Error', err)  # err is exception description
+else:
+  print('The result is ', result)
+finally:
+  print("Thank you for using the service")
+
+# line num of exception
+import sys 
+e_type, e_object, e_traceback = sys.exc_info()
+e_line = e_traceback.tb_lineno
+
+
+# exception name
+# use __class__.__name__ called on err object
+
+# multiple exceptions
+# except (ZeroDivisionError, TypeError) as err:
