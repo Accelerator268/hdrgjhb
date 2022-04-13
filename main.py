d05e751c3df693d0c7c6e4a0c231415839db70e1
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-13 00:29:30 +0000

    map & filter (2)

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-n = int(input())
-if n % 2 == 0:
-  print(n//2)
-else:
-  print(n//2+1)
+num = [i for i in range(10)]
+even = list(filter(lambda x: x%2==0, num))
+odd = list(filter(lambda x: x%2, num))
+
+my_sum = 0
+i = 0
+my_sum = list(map(lambda x, y: x + y, even, odd))
+
+reminders = list(map(lambda x: x%3, my_sum))
+
+nonzero_reminders = list(filter(lambda x: x, reminders))
+
+print(nonzero_reminders)
