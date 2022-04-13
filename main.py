ac77a6fae42ffcdae0d42125b9a6d7041fa9bcf0
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-13 23:24:42 +0000

    iterator(3)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'April', 'September', 'October', 'November', 'December']
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 revenues = [randint(500, 2000) for _ in range(12)]
 profit = ['n/a']
 conclusion = ['n\a', 'great', 'decent', 'need follow up', 'critical']
@@ -10,14 +10,14 @@ for i in range(1, 12):
   profit.append('%.1f' % ((revenues[i] - revenues[i-1])*100/revenues[i]))
 
 for i in range(12):
-  print(months[i], revenues[i], profit[i]+'%')
+  print(months[i], revenues[i], profit[i], end = '')
   if i == 0:
     print(conclusion[0])
-  elif profit[i]>50:
-    print(conclusion[1])
-  elif 50>=profit[i]>=25:
-    print(conclusion[2])
-  elif 25>profit[i]>=0:
-    print(conclusion[3])
-  elif profit[i]<0:
-    print(conclusion[4])
+  elif revenues[i]/revenues[i-1] > 1.5:
+    print('%', conclusion[1])
+  elif 1.5 >= revenues[i]/revenues[i-1] >= 1.25:
+    print('%', conclusion[2])
+  elif 1.25 > revenues[i]/revenues[i-1] >= 1:
+    print('%', conclusion[3])
+  elif revenues[i]/revenues[i-1] < 1:
+    print('%', conclusion[4])
