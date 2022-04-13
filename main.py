b7aa85227156a8a01b2a1798289cd0d0ddac6411
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-13 02:16:18 +0000

    map & filter (4)

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-from random import randint
-
-student_list = ['Denis', 'Dima', '???', 'Dima G.', 'Dima T.', 'Alexey', 'X3', 'NN']
-student_points_math = [randint(25, 50) for _ in range(len(student_list))]
-student_points_phys = [randint(25, 50) for _ in range(len(student_list))]
-student_points_eng = [randint(25, 50) for _ in range(len(student_list))]
-student_pass = list(map(lambda x, y, z: x + y + z, student_points_math, student_points_phys, student_points_eng))
-print(student_list)
-print(student_pass)
-print([student_list[i] for i in range(len(student_pass)) if student_pass[i]>=100])
+inputs = [i for i in range(10)]
+results = [10 * x for x in inputs]
+results = list(map(lambda x: x * 10, inputs))
