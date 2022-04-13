d6d091e46b4d5985934587618590488e97cf75c9
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-13 02:20:24 +0000

    map & filter (5)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-inputs = [i for i in range(10)]
-results = [10 * x for x in inputs]
-results = list(map(lambda x: x * 10, inputs))
+daily_temp_c = [20.5, 19, 15, 25, 27, 30, 31, 29, 26, 21,
+                19, 25, 27.5, 28, 26, 29.5, 31, 27.5, 26, 29,
+                18, 17.5, 17, 16.5, 19, 20, 25, 26.5, 27, 28,
+                20.5, 19, 25, 27.5, 28, 26, 15, 25, 27, 28]
+print(len(list(filter(lambda x: x*9/5+32>80, daily_temp_c))))
