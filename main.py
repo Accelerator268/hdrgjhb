e63dcb4faac3a8e3637c57b4a3963a88cc8c56fa
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-18 01:13:43 +0000

    most frequent

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-a, b = int(input()), int(input())
+text = '''I've become so numb
+I can't feel you there
+Become so tired
+So much more aware
+I'm becoming this
+All I want to do
+Is be more like me
+And be less like you
+And I know
+I may end up failing too
+But I know
+You were just like me with someone disappointed in you'''
 
-while True:
-  try:
-    a/b
-  except ZeroDivisionError:
-    print('Error')
-  else:
-    print(a/b)
-    break
-  finally:
-    b+=2
+text_list = text.split()
+
+# set default method
+
+freq_dict = {}
+
+
+from collections import defaultdict, Counter
+
+  
+freq_dict = defaultdict(int)
+
+for word in text_list:
+  freq_dict[word] += 1
+
+print(freq_dict['python'])
+
+freq_counter = Counter(text_list)
+print(freq_counter.most_common(10))
