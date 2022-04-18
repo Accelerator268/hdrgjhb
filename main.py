370a0528b664c73a86535ea27f22956d2a765aef
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-18 01:10:33 +0000

    handling (8)

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import sys
+a, b = int(input()), int(input())
 
-
-try:
-  a = int(input())
-  b = int(input())
-  print(a/b, c)
-except ZeroDivisionError:
-  e_type, e_object, e_traceback = sys.exc_info()
-  print('ZeroDivisionError exception on line', e_traceback.tb_lineno, end = '!')
-except NameError as err:
-  e_type, e_object, e_traceback = sys.exc_info()
-  print('NameError exception on line', e_traceback.tb_lineno, end = '!')
-except ValueError as err:
-  e_type, e_object, e_traceback = sys.exc_info()
-  print('ValueError exception on line', e_traceback.tb_lineno, end = '!')
+while True:
+  try:
+    a/b
+  except ZeroDivisionError:
+    print('Error')
+  else:
+    print(a/b)
+    break
+  finally:
+    b+=2
