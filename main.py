46e4a5b50677ce822ddf926157ce15abcae3a1d9
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-18 01:15:04 +0000

    custom (1)

--- main.py
+++ main.py
@@ -1,32 +1,10 @@
-text = '''I've become so numb
-I can't feel you there
-Become so tired
-So much more aware
-I'm becoming this
-All I want to do
-Is be more like me
-And be less like you
-And I know
-I may end up failing too
-But I know
-You were just like me with someone disappointed in you'''
+#Something went wrong!
 
-text_list = text.split()
+a = -2
+b = 10
+c = a / b
 
-# set default method
-
-freq_dict = {}
-
-
-from collections import defaultdict, Counter
-
-  
-freq_dict = defaultdict(int)
-
-for word in text_list:
-  freq_dict[word] += 1
-
-print(freq_dict['python'])
-
-freq_counter = Counter(text_list)
-print(freq_counter.most_common(10))
+if c >= 0:
+    print(c)
+else:
+    raise Exception('Something went wrong!')
