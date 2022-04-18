c8cf7a62bcebf1b5103f4b83f80717a690d27ff8
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-18 00:20:11 +0000

    handling (4)

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-#linear search
-lst = [1, 3, 6, 8, 9, 12, 13, 19]
-num = 0
-k = 1
-for i in range(len(lst)):
-  for j in range(i, len(lst)):
-    if lst[j] - lst[i] >= 3:
-      num += 1
-      print(lst[i], lst[j])
-      k += 1
+a, b = int(input()), int(input())
 
-print(num, k)
-
-
-#two_pointer search
-i = j = 0
-num = 0
-k = 1
-while i < len(lst) and j < len(lst):
-  if lst[j] - lst[i] < 3:
-    j += 1
-  else:
-    num += len(lst) - j
-    i += 1
-  k += 1
-print(num, k)
-    
-    
+try:
+  print(a/b)
+except ZeroDivisionError:
+  print('The Error!')
