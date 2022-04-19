6e77f86d57c0fb5ef14373be9521ed8d2a883983
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-19 01:25:51 +0000

    custom (3)

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-class NegativeSumError(Exception):
+def sum_with_exceptions(a, b):
+
+  class NegativeSumError(Exception):
     def __str__(self):
-        return 'Sum is negative'
+      return 'Sum is negative'
+
+  if a+b < 0:
+    raise NegativeSumError
+  else:
+    print(a+b)
 
 
 a, b = int(input()), int(input())
-if a+b < 0:
-    raise NegativeSumError
-else:
-  print(a+b)
+sum_with_exceptions(a, b)
