e0ddf3d717552e02f0b032c6f92ce143063580d3
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-20 02:52:44 +0000

    two pointer (2) BETA

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 ND = input().split()
 n, d = int(ND[0]), int(ND[1])
 list = [int(i) for i in input().split()]
+reversed_list = list.copy()
+reversed_list.reverse()
+print(list, reversed_list)
 count = 0
 for i in range(len(list)):
-  if list[-1]-4>list[i]:
-    for j in range(i+1, len(list)):
-      if list[j]-4>=list[i]:
+  if list[i] == d:
+    list[i] = 0
+    reversed_list[-i-1] = 0
+    count += 1
+  elif list[i]<d:
+    for j in reversed(list):
+      if list[j] != 0 and list[i]+list[j] <= d:
+        list[i], list[j] = 0, 0
         count+=1
-  elif list[-1]-4==list[i]:
-    count+=1
 
 print(count)
