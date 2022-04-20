a611b537c38293c5bc70db88ed5370fb20434a2b
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-20 00:50:44 +0000

    reduce & counter

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-import socket
+from functools import reduce
+from collections import Counter
 
 
-with socket.socket() as client_socket:
+stock = [[0, 6], [3, 4], [1, 4], [4, 5], [3, 5], [1, 2], [1, 3], [0, 1], [5, 6], [6, 6], [0, 4], [1, 1], [2, 3], [0, 3], [3, 3], [2, 4], [1, 5], [2, 6], [3, 6], [0, 2], [4, 4]]
 
-  hostname = '127.0.0.1'  # str
-  port = 5000  # int
 
-  address = (hostname, port)
-  
-  client_socket.connect(address)  # one arg!!
-  
-  data = 'This is my test string'
-  
-  client_socket.send(data.encode())  # converting to bytes
-  
-  buffer = 1024  # response limit
-  response = client_socket.recv(buffer)
-  response = response.decode()  # converting back to rea
+new_stock = reduce(lambda x, y: x + y, stock)
+print(new_stock)
 
-  print(response)
+
+new_new_stock = [item for sublist in stock for item in sublist]
+print(new_new_stock)
