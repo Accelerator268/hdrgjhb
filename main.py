6cb71390912b0ff25d1dad31090504b1b2a837b9
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-20 02:22:50 +0000

    two pointer (1)

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-from string import digits
+ND = input().split()
+n, d = int(ND[0]), int(ND[1])
+list = [int(i) for i in input().split()]
+count = 0
+for i in range(len(list)):
+  if list[-1]-4>list[i]:
+    for j in range(i+1, len(list)):
+      if list[j]-4>=list[i]:
+        count+=1
+  elif list[-1]-4==list[i]:
+    count+=1
 
-
-class NotWordError(Exception):
-    def __init__(self, word):
-        self.message = f'%s is not a word, sorry!' % word
-        super().__init__(self.message)
-
-
-def check_word(word):
-    for i in range(len(word)):
-        if word[i] in digits:
-            raise NotWordError(word)  # raise only once!!!
-    return word
-
-
-def error_handling(word):
-    try:
-        print(check_word(word))   # print positive
-    except NotWordError as err:
-        print(err)                # print negative not raise again :)))
-
-
-word = input()
-error_handling(word)
+print(count)
