a5028a53328c0e1fbd078dad4785b3fc9e3267c6
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-20 00:57:58 +0000

    custom (7)

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from functools import reduce
-from collections import Counter
+#B
+class DateDelimiterError(Exception):
+    def __str__(self):
+        return "You should use the '/' sign as a delimiter."
 
 
-stock = [[0, 6], [3, 4], [1, 4], [4, 5], [3, 5], [1, 2], [1, 3], [0, 1], [5, 6], [6, 6], [0, 4], [1, 1], [2, 3], [0, 3], [3, 3], [2, 4], [1, 5], [2, 6], [3, 6], [0, 2], [4, 4]]
+date = input()
+if '/' not in date:
+    raise DateDelimiterError
+else:
+    print(date)
 
 
-new_stock = reduce(lambda x, y: x + y, stock)
-print(new_stock)
+
+#D
+class DateDelimiterError(Exception):
+    pass
 
 
-new_new_stock = [item for sublist in stock for item in sublist]
-print(new_new_stock)
+date = input()
+if '/' not in date:
+    raise DateDelimiterError("You should use the '/' sign as a delimiter.")
+else:
+    print(date)
