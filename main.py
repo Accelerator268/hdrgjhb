929c8044321ec860d82504f5ecf523395287d997
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-21 00:13:25 +0000

    chain, product, combinations

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-ND = input().split()
-n, d = int(ND[0]), int(ND[1])
-list = [int(i) for i in input().split()]
-reversed_list = list.copy()
-reversed_list.reverse()
-count = 0
-for i in range(len(list)):
-  if list[i] == d:
-    list[i] = 0
-    reversed_list[-i-1] = 0
-    count += 1
-  elif list[i]<d:
-    for j in range(i, len(reversed_list)):
-      if reversed_list[j] != 0 and list[i]+reversed_list[j] <= d:
-        list[i], list[-j-1], reversed_list[j], reversed_list[-i-1] = 0, 0, 0, 0
-        count+=1
+# digits = 'abc'
 
-print(count)
+import itertools
+
+# lst1, lst2, lst3 = [1, 2], [3, 4], [5, 6]
+
+# # chain
+
+
+# for num in itertools.chain(lst1, lst2, lst3):
+#   print(num)
+
+# # product
+
+# first = ['Hi', 'Bye']
+# second = ['Anton', 'Anna']
+
+# my_gen = (opt for i in range(1, 4) for opt in itertools.product(digits, repeat=i))
+ 
+
+# for opt in my_gen:
+#   print(''.join(opt))
+
+
+# combinations
+
+flowers = ['a1', 'a2', 'a3', 'a4']
+
+my_iter = itertools.combinations(flowers, 3)
+
+for opt in my_iter:
+  print(opt)
