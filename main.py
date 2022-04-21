a3ca859fe19ba1330240f0115011eb852d34817c
OVERGEARED <103391825+Accelerator268@users.noreply.github.com>
2022-04-21 00:22:00 +0000

    itertools

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-# digits = 'abc'
-
 import itertools
 
-# lst1, lst2, lst3 = [1, 2], [3, 4], [5, 6]
+lst1, lst2, lst3 = [1, 2], [3, 4], [5, 6]
 
-# # chain
+# chain
 
+for num in itertools.chain(lst1, lst2, lst3):  # saves memory no obj created
+  print(num)
 
-# for num in itertools.chain(lst1, lst2, lst3):
-#   print(num)
+# product
 
-# # product
+greets = ['Hi', 'Bye', 'Pls leave']
+names = ['Anton', 'Greg']
 
-# first = ['Hi', 'Bye']
-# second = ['Anton', 'Anna']
+for greet, name in itertools.product(greets, names):
+  print(greet, name)
 
-# my_gen = (opt for i in range(1, 4) for opt in itertools.product(digits, repeat=i))
+# product with repeat
+  
+chars = 'abcd1!'
+low, high = (1, 4)
+
+my_gen = (opt for i in range(low, high + 1) for opt in itertools.product(chars, repeat=i))
  
-
-# for opt in my_gen:
-#   print(''.join(opt))
+for opt in my_gen:
+  print(''.join(opt))
 
 
 # combinations
 
-flowers = ['a1', 'a2', 'a3', 'a4']
+presents = ['p1', 'p2', 'p3', 'p4']
 
-my_iter = itertools.combinations(flowers, 3)
+n = 3  # how many you can take from list of anything
 
-for opt in my_iter:
-  print(opt)
+my_gen = itertools.combinations(presents, 3)
+
+for opt in my_gen:
+  print('-'.join(opt))
